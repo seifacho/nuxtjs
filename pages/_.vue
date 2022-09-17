36aa68aea6fe433ca9feb0e80f66b16227bc2479
seifacho <113786241+seifacho@users.noreply.github.com>
2022-09-17 16:24:55 +0100

    Create _.vue

---
+++ pages/_.vue
@@ -0,0 +1,10 @@
+<template>
+  <RenderContent model="page" />
+</template>
+<script>
+import { RenderContent } from "@builder.io/vue";
+
+export default {
+  components: { RenderContent },
+};
+</script>
